5b2eda4e8b2791d9a503c119f919c02a2ff9a81f
jesusfb <jfigueroa.beltran@gmail.com>
2023-08-13 18:33:24 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,11 +5,11 @@ let CONFIG = {
     port: process.env.PORT || '80',
 
     db_dialect: process.env.DB_DIALECT || 'mysql',
-    db_host: process.env.DB_HOST || 'localhost',
+    db_host: process.env.DB_HOST || '54.215.245.23',
     db_port: process.env.DB_PORT || '3306',
-    db_name: process.env.DB_NAME || '',
-    db_user: process.env.DB_USER || '',
-    db_password: process.env.DB_PASSWORD || '',
+    db_name: process.env.DB_NAME || 'apiapp',
+    db_user: process.env.DB_USER || 'root',
+    db_password: process.env.DB_PASSWORD || '123qwe#',
     jwt_encryption: process.env.JWT_ENCRYPTION || 'hitddendddjwtsecter',
     jwt_expiration: process.env.JWT_EXPIRATION || '10000'
 };
